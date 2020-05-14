0dc3a667284266df1fe3b79930fa450236fc7cd1
Fan Qi <353208262@qq.com>
2020-05-15 01:22:02 +0800

    feat: 使用viper读取配置文件

--- main.go
+++ main.go
@@ -3,9 +3,26 @@ package main
 import (
 	_ "ExpoUponCloud/routers"
 	"github.com/astaxie/beego"
+	"github.com/spf13/viper"
+	"log"
+	"fmt"
 )
 
+func init() {
+	viper.SetConfigName("config") // 读取json配置文件
+	viper.AddConfigPath("conf")      // 设置配置文件和可执行二进制文件在用一个目录
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			log.Println("no such config file")
+		} else {
+			log.Println("read config error")
+		}
+		log.Fatal(err) // 读取配置文件失败致命错误
+	}
+}
+
 func main() {
+	fmt.Println(viper.GetString("mysql.url"))
 	beego.Run()
 }
 
