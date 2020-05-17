02d460e88f1dc408966821932e34377ff9cb686b
Fan Qi <353208262@qq.com>
2020-05-17 15:11:01 +0800

    feat: 改写配置读取

--- main.go
+++ main.go
@@ -3,36 +3,19 @@ package main
 import (
 	_ "ExpoUponCloud/routers"
 	"github.com/astaxie/beego"
-	"github.com/spf13/viper"
 	"fmt"
-	"log"
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func init() {
-	viperInit()
-
+	// 连接mysql
 	orm.RegisterDriver("mysql", orm.DRMySQL)
 	orm.RegisterDataBase("default", "mysql", fmt.Sprintf("%s:%s@(%s)/expodb?"+
-		"charset=utf8&loc=Local", viper.GetString("mysql.username"), viper.GetString("mysql.password"),
-		viper.GetString("mysql.url")))
+		"charset=utf8&loc=Local", beego.AppConfig.String("mysqluser"), beego.AppConfig.String("mysqlpass"),
+		beego.AppConfig.String("mysqlurl")))
 }
 
 func main() {
-	fmt.Println(viper.GetString("mysql.url"))
 	beego.Run()
-}
-
-func viperInit() {
-	viper.SetConfigName("config") // 读取json配置文件
-	viper.AddConfigPath("conf")   // 设置配置文件和可执行二进制文件在用一个目录
-	if err := viper.ReadInConfig(); err != nil {
-		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			log.Println("no such config file")
-		} else {
-			log.Println("read config error")
-		}
-		log.Fatal(err) // 读取配置文件失败致命错误
-	}
 }
